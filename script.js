422db69be9548f04b89815a6759f312057f78349
Godana <godanaemiru@gmail.com>
2022-10-04 13:36:15 +0300

    declared variables and initialised functions

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
-const pw = document.getElementById("pw");
-const copy = document.getElementById("copy");
-const len = document.getElementById("len");
-const upper = document.getElementById("upper");
-const lower = document.getElementById("lower");
-const number = document.getElementById("number");
-const symbol = document.getElementById("symbol");
-const generate = document.getElementById("generate");
+const pwEl = document.getElementById("pw");
+const copyEl = document.getElementById("copy");
+const lenEl = document.getElementById("len");
+const upperEl = document.getElementById("upper");
+const lowerEl = document.getElementById("lower");
+const numberEl = document.getElementById("number");
+const symbolEl = document.getElementById("symbol");
+const generateEl = document.getElementById("generate");
+
+const upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerLetters = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const symbols = '!@#$%^&*()_+=';
+
+
+function getLowerCase() {
+    return lowerLetters[Math.floor(Math.random() * lowerLetters.length)]
+}
+
+function getUpperCase() {
+    return upperLetters[Math.floor(Math.random() * upperLetters.length)]
+}
+
+function getNumber() {
+    return numbers[Math.floor(Math.random() * numbers.length)]
+}
+
+function getSymbol() {
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
+
+function generatePassword() {
+
+    pwEl.innerText = password;
+}
+
+generateEl.addEventListener('click', generatePassword);
+
